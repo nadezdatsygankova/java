2da4ed6627e5650f45d6bc5700a5e85e5e38086c
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-10-02 01:03:05 -0400

    Update Language.java

--- codecademy/Language/Language.java
+++ codecademy/Language/Language.java
@@ -15,18 +15,27 @@ class Language{
        System.out.println(this.name + "is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken +".");
        System.out.println("The language follows the word order: " + this.wordOrder +".");
   }
+  public void setRigionsSpoken(String regionSpoken){
+   this.regionsSpoken = regionsSpoken;
+  }
+
+  public void setNumSpeakers(int numSpeakers){
+   this.numSpeakers = numSpeakers;
+  }
+   public void setWordOrder(String wordOrder){
+   this.wordOrder = wordOrder;
+  }
 
   public static void main (String[] args){
+
     Language eng = new Language ("English", 2000000, "Canada", "subject-verb-object");
-    eng.getInfo();
-
     Mayan chontal = new Mayan("Chontal", 36810);
-    chontal.getInfo(); 
-
     SinoTibetan chinese = new SinoTibetan("Mandarin Chinese", 920000000);
     SinoTibetan burmese = new SinoTibetan("Burmese", 33000000);
-    chinese.getInfo(); 
-    burmese.getInfo(); 
 
+    Language[] languages = {eng,chontal,chinese,burmese};
+    for(Language language: languages){
+      language.getInfo();
+    }
   }
 }
