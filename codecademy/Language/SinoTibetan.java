a393039db95bb9d3246c627f36c7b0f5564e9b74
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-10-02 00:56:00 -0400

    Create SinoTibetan.java

---
+++ codecademy/Language/SinoTibetan.java
@@ -0,0 +1,8 @@
+class SinoTibetan extends Language{
+ public SinoTibetan(String name, int numSpeakers){
+   super(name, numSpeakers,"Asia", "subject-object-verb");
+   if(name.contains("Chinese")){
+     this.wordOrder ="subject-verb-object";
+   }
+ }
+}
